aa486ad93aa877088112be814f287d08135b379e
hoh miyazawa <hohMiyazawa@yandex.com>
2021-05-25 13:24:56 +0200

    adding missing unfilter for semi_paeth

--- unfilters.hpp
+++ unfilters.hpp
@@ -47,6 +47,49 @@ void unfilter_all_ffv1(uint8_t* in_bytes, uint32_t range, uint32_t width, uint32
 	}
 }
 
+void unfilter_all_sPaeth(uint8_t* in_bytes, uint32_t range, uint32_t width, uint32_t height){
+	if(range == 256){//8bit overflow allowed
+		for(size_t i=1;i<width;i++){
+			in_bytes[i] = in_bytes[i] + in_bytes[i - 1];//top edge is always left-predicted
+		}
+		for(size_t y=1;y<height;y++){
+			in_bytes[y * width] = in_bytes[y * width] + in_bytes[(y-1) * width];//left edge is always top-predicted
+			for(size_t i=1;i<width;i++){
+				uint8_t L = in_bytes[y * width + i - 1];
+				uint8_t TL = in_bytes[(y-1) * width + i - 1];
+				uint8_t T = in_bytes[(y-1) * width + i];
+				in_bytes[(y * width) + i] = in_bytes[y * width + i] + semi_paeth(
+					L,
+					T,
+					TL
+				);
+			}
+		}
+	}
+	else{
+		for(size_t i=1;i<width;i++){
+			in_bytes[i] = add_mod(in_bytes[i],in_bytes[i - 1],range);//top edge is always left-predicted
+		}
+		for(size_t y=1;y<height;y++){
+			in_bytes[y * width] = add_mod(in_bytes[y * width],in_bytes[(y-1) * width],range);//left edge is always top-predicted
+			for(size_t i=1;i<width;i++){
+				uint8_t L = in_bytes[y * width + i - 1];
+				uint8_t TL = in_bytes[(y-1) * width + i - 1];
+				uint8_t T = in_bytes[(y-1) * width + i];
+				in_bytes[(y * width) + i] = add_mod(
+					in_bytes[y * width + i],
+					semi_paeth(
+						L,
+						T,
+						TL
+					),
+					range
+				);
+			}
+		}
+	}
+}
+
 void unfilter_all_median(uint8_t* in_bytes, uint32_t range, uint32_t width, uint32_t height){
 	if(range == 256){//8bit overflow allowed
 		for(size_t i=1;i<width;i++){
@@ -215,6 +258,9 @@ void unfilter_all(
 	}
 	else if(predictor == 6){
 		unfilter_all_median(in_bytes, range, width, height);
+	}
+	else if(predictor == 6){
+		unfilter_all_sPaeth(in_bytes, range, width, height);
 	}
 	else if(predictor == 68){
 		unfilter_all_left(in_bytes, range, width, height);
