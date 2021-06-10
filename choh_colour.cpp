cd6112673f8cc52778d61b04beeeb5be28c4052c
hoh miyazawa <hohMiyazawa@yandex.com>
2021-06-10 10:19:09 +0200

    experimental colour encoders

--- choh_colour.cpp
+++ choh_colour.cpp
@@ -20,7 +20,7 @@
 #include "table_encode.hpp"
 
 void print_usage(){
-	printf("./choh_colour infile.png outfile.hoh speed\n\nspeed must be 1 to 4\nThis is an experimental colour version and it sucks.\n");
+	printf("./choh_colour infile.png outfile.hoh speed\n\nspeed must be 1 to 5\nThis is an experimental colour version and it sucks.\n");
 }
 
 void colour_encode_entropy(uint8_t* in_bytes,size_t range,uint32_t width,uint32_t height,uint8_t*& outPointer){
@@ -274,6 +274,209 @@ void colour_encode_ffv1(uint8_t* in_bytes,size_t range,uint32_t width,uint32_t h
 	*(--outPointer) = 0b10000110;
 }
 
+void colour_encode_ffv1_quad(uint8_t* in_bytes,size_t range,uint32_t width,uint32_t height,uint8_t*& outPointer){
+	printf("filtering colour channels...\n");
+	uint8_t* filtered_bytes = colourSub_filter_all_ffv1(in_bytes, range, width, height);
+
+	uint8_t contextNumber = 12;
+
+	uint8_t* entropy_image = new uint8_t[contextNumber];
+	for(size_t i=0;i<contextNumber;i++){
+		entropy_image[i] = i;
+	}
+
+	uint32_t entropyWidth = 2;
+	uint32_t entropyHeight = 2;
+
+
+	uint32_t entropyWidth_block  = (width + entropyWidth - 1)/entropyWidth;
+	uint32_t entropyHeight_block  = (height + entropyHeight - 1)/entropyHeight;
+	printf("entropy dimensions %d x %d\n",(int)entropyWidth,(int)entropyHeight);
+
+	SymbolStats* statistics = new SymbolStats[contextNumber];
+	
+	for(size_t context = 0;context < contextNumber;context++){
+		SymbolStats stats;
+		statistics[context] = stats;
+		for(size_t i=0;i<256;i++){
+			statistics[context].freqs[i] = 0;
+		}
+	}
+	for(size_t i=0;i<width*height;i++){
+		uint8_t cntr = entropy_image[tileIndexFromPixel(
+			i,
+			width,
+			entropyWidth,
+			entropyWidth_block,
+			entropyHeight_block
+		)];
+		statistics[cntr*3].freqs[filtered_bytes[i*3]]++;
+		statistics[cntr*3 + 1].freqs[filtered_bytes[i*3 + 1]]++;
+		statistics[cntr*3 + 2].freqs[filtered_bytes[i*3 + 2]]++;
+	}
+
+	BitWriter tableEncode;
+	SymbolStats table[contextNumber];
+	for(size_t context = 0;context < contextNumber;context++){
+		table[context] = encode_freqTable(statistics[context],tableEncode, range);
+	}
+	delete[] statistics;
+	tableEncode.conclude();
+
+	RansEncSymbol esyms[contextNumber][256];
+
+	for(size_t cont=0;cont<contextNumber;cont++){
+		for(size_t i=0; i < 256; i++) {
+			RansEncSymbolInit(&esyms[cont][i], table[cont].cum_freqs[i], table[cont].freqs[i], 16);
+		}
+	}
+
+	RansState rans;
+	RansEncInit(&rans);
+	for(size_t index=width*height;index--;){
+		uint8_t cntr = entropy_image[tileIndexFromPixel(
+			index,
+			width,
+			entropyWidth,
+			entropyWidth_block,
+			entropyHeight_block
+		)];
+		RansEncPutSymbol(&rans, &outPointer, esyms[cntr*3 + 2] + filtered_bytes[index*3 + 2]);
+		RansEncPutSymbol(&rans, &outPointer, esyms[cntr*3 + 1] + filtered_bytes[index*3 + 1]);
+		RansEncPutSymbol(&rans, &outPointer, esyms[cntr*3 + 0] + filtered_bytes[index*3 + 0]);
+	}
+	RansEncFlush(&rans, &outPointer);
+	delete[] filtered_bytes;
+
+	uint8_t* trailing = outPointer;
+	for(size_t i=tableEncode.length;i--;){
+		*(--outPointer) = tableEncode.buffer[i];
+	}
+	printf("entropy table size: %d bytes\n",(int)(trailing - outPointer));
+
+	trailing = outPointer;
+	colour_encode_entropy(
+		entropy_image,
+		contextNumber,
+		entropyWidth,
+		entropyHeight,
+		outPointer
+	);
+	delete[] entropy_image;
+	printf("---entropy image size: %d bytes\n",(int)(trailing - outPointer));
+	writeVarint_reverse((uint32_t)(entropyHeight - 1),outPointer);
+	writeVarint_reverse((uint32_t)(entropyHeight - 1), outPointer);
+
+	*(--outPointer) = contextNumber - 1;
+
+	*(--outPointer) = 0b00000000;//one predictor: ffv1
+	*(--outPointer) = 0b00000000;
+	*(--outPointer) = 1 - 1;
+
+	*(--outPointer) = 0b10000110;
+}
+
+void colour_encode_ffv1_4x4(uint8_t* in_bytes,size_t range,uint32_t width,uint32_t height,uint8_t*& outPointer){
+	printf("filtering colour channels...\n");
+	uint8_t* filtered_bytes = colourSub_filter_all_ffv1(in_bytes, range, width, height);
+	uint32_t entropyWidth = 4;
+	uint32_t entropyHeight = 4;
+
+	uint8_t contextNumber = entropyWidth*entropyHeight*3;
+
+	uint8_t* entropy_image = new uint8_t[contextNumber];
+	for(size_t i=0;i<contextNumber;i++){
+		entropy_image[i] = i;
+	}
+
+
+	uint32_t entropyWidth_block  = (width + entropyWidth - 1)/entropyWidth;
+	uint32_t entropyHeight_block  = (height + entropyHeight - 1)/entropyHeight;
+	printf("entropy dimensions %d x %d\n",(int)entropyWidth,(int)entropyHeight);
+
+	SymbolStats* statistics = new SymbolStats[contextNumber];
+	
+	for(size_t context = 0;context < contextNumber;context++){
+		SymbolStats stats;
+		statistics[context] = stats;
+		for(size_t i=0;i<256;i++){
+			statistics[context].freqs[i] = 0;
+		}
+	}
+	for(size_t i=0;i<width*height;i++){
+		uint8_t cntr = entropy_image[tileIndexFromPixel(
+			i,
+			width,
+			entropyWidth,
+			entropyWidth_block,
+			entropyHeight_block
+		)];
+		statistics[cntr*3].freqs[filtered_bytes[i*3]]++;
+		statistics[cntr*3 + 1].freqs[filtered_bytes[i*3 + 1]]++;
+		statistics[cntr*3 + 2].freqs[filtered_bytes[i*3 + 2]]++;
+	}
+
+	BitWriter tableEncode;
+	SymbolStats table[contextNumber];
+	for(size_t context = 0;context < contextNumber;context++){
+		table[context] = encode_freqTable(statistics[context],tableEncode, range);
+	}
+	delete[] statistics;
+	tableEncode.conclude();
+
+	RansEncSymbol esyms[contextNumber][256];
+
+	for(size_t cont=0;cont<contextNumber;cont++){
+		for(size_t i=0; i < 256; i++) {
+			RansEncSymbolInit(&esyms[cont][i], table[cont].cum_freqs[i], table[cont].freqs[i], 16);
+		}
+	}
+
+	RansState rans;
+	RansEncInit(&rans);
+	for(size_t index=width*height;index--;){
+		uint8_t cntr = entropy_image[tileIndexFromPixel(
+			index,
+			width,
+			entropyWidth,
+			entropyWidth_block,
+			entropyHeight_block
+		)];
+		RansEncPutSymbol(&rans, &outPointer, esyms[cntr*3 + 2] + filtered_bytes[index*3 + 2]);
+		RansEncPutSymbol(&rans, &outPointer, esyms[cntr*3 + 1] + filtered_bytes[index*3 + 1]);
+		RansEncPutSymbol(&rans, &outPointer, esyms[cntr*3 + 0] + filtered_bytes[index*3 + 0]);
+	}
+	RansEncFlush(&rans, &outPointer);
+	delete[] filtered_bytes;
+
+	uint8_t* trailing = outPointer;
+	for(size_t i=tableEncode.length;i--;){
+		*(--outPointer) = tableEncode.buffer[i];
+	}
+	printf("entropy table size: %d bytes\n",(int)(trailing - outPointer));
+
+	trailing = outPointer;
+	colour_encode_entropy(
+		entropy_image,
+		contextNumber,
+		entropyWidth,
+		entropyHeight,
+		outPointer
+	);
+	delete[] entropy_image;
+	printf("---entropy image size: %d bytes\n",(int)(trailing - outPointer));
+	writeVarint_reverse((uint32_t)(entropyHeight - 1),outPointer);
+	writeVarint_reverse((uint32_t)(entropyWidth - 1), outPointer);
+
+	*(--outPointer) = contextNumber - 1;
+
+	*(--outPointer) = 0b00000000;//one predictor: ffv1
+	*(--outPointer) = 0b00000000;
+	*(--outPointer) = 1 - 1;
+
+	*(--outPointer) = 0b10000110;
+}
+
 int main(int argc, char *argv[]){
 	if(argc < 4){
 		printf("not enough arguments\n");
@@ -317,8 +520,14 @@ int main(int argc, char *argv[]){
 	else if(speed == 4){
 		colour_encode_ffv1(alpha_stripped, 256,width,height,outPointer);
 	}
+	else if(speed == 5){
+		colour_encode_ffv1_quad(alpha_stripped, 256,width,height,outPointer);
+	}
+	else if(speed == 6){
+		colour_encode_ffv1_4x4(alpha_stripped, 256,width,height,outPointer);
+	}
 	else{
-		panic("speed must be 1 to 4!\n");
+		panic("speed must be 1 to 5!\n");
 	}
 	delete[] alpha_stripped;
 
