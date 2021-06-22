f152f72fe2516ad81db1db7308220f5ee2b67918
hoh miyazawa <hohMiyazawa@yandex.com>
2021-06-22 22:19:31 +0200

    prefix coding

--- lz_optimiser.hpp
+++ lz_optimiser.hpp
@@ -3,19 +3,32 @@
 
 #include "prefix_coding.hpp"
 
-uint16_t* lz_dist2(
+lz_triple* lz_dist(
 	uint8_t* in_bytes,
 	uint32_t width,
 	uint32_t height,
-	size_t& lz_pro_size,
+	size_t& lz_size,
 	size_t speed
 ){
-	uint32_t lz_data[width*height + 64];
-	size_t lz_size = 0;
+	lz_triple* lz_data = new lz_triple[width*height];
 
 	size_t limit = (64 << speed);
 
+	size_t extrabits = 0;
+	uint8_t max_back_x = inverse_prefix(width/2)*2 + 1;
+
 	size_t previous_match = 0;
+
+	lz_triple firstTriple;
+	lz_data[0] = firstTriple;
+	lz_size = 1;
+	uint32_t bitbuffer = 0;
+	uint8_t bitbuffer_length = 0;
+
+	uint32_t cache_future = 0;
+	uint32_t cache_back_x = 0;
+	uint32_t cache_back_y = 0;
+	uint32_t cache_matchlen = 0;
 
 	for(int i=0;i<width*height;){
 		size_t match_length = 0;
@@ -66,50 +79,70 @@ uint16_t* lz_dist2(
 			previous_match += 1;
 		}
 		else{
-			lz_data[lz_size++] = previous_match;
+			back_ref -= 1;
+			match_length -= 1;
+			uint8_t future_prefix = inverse_prefix(previous_match);
+			uint8_t future_extrabits = extrabits_from_prefix(future_prefix);
+			uint32_t future_extrabits_value = prefix_extrabits(previous_match);
+			if(previous_match == cache_future){
+				lz_data[lz_size - 1].future = 0;
+			}
+			else{
+				lz_data[lz_size - 1].future = future_prefix + 1;
+				cache_future = previous_match;
+			}
+
+			uint32_t back_x = back_ref % width;
+			uint32_t back_y = back_ref / width;
+			uint8_t back_x_prefix;
+			if(width - back_x < back_x){
+				back_x_prefix = max_back_x - inverse_prefix(back_x);
+			}
+			else{
+				back_x_prefix = inverse_prefix(back_x) + 1;
+			}
+			if(back_x == cache_back_x){
+				lz_data[lz_size].backref_x = 0;
+			}
+			else{
+				lz_data[lz_size].backref_x = back_x_prefix;
+				cache_back_x = back_x;
+			}
+			uint8_t back_y_prefix = inverse_prefix(back_y);
+			if(back_y == cache_back_y){
+				lz_data[lz_size].backref_y = 0;
+			}
+			else{
+				lz_data[lz_size].backref_y = back_y_prefix + 1;
+				cache_back_y = back_y;
+			}
+
+			uint8_t matchlen_prefix = inverse_prefix(match_length);
+			if(match_length == cache_matchlen){
+				lz_data[lz_size].matchlen = 0;
+			}
+			else{
+				lz_data[lz_size].matchlen = matchlen_prefix + 1;
+				cache_matchlen = match_length;
+			}
+
+			lz_size++;
 			previous_match = 0;
-			lz_data[lz_size++] = back_ref - 1;
-			lz_data[lz_size++] = match_length - 1;
 			i += match_length;
 		}
 	}
-	lz_data[lz_size++] = previous_match;
-
-	lz_pro_size = 0;
-	uint16_t* lz_data_processed = new uint16_t[width*height + 64];
-
-	size_t extrabits = 0;
-
-	uint8_t max_back_x = inverse_prefix(width/2)*2 + 1;
-
-	for(size_t i=0;i<(lz_size - 1);i+=3){
-		uint8_t future_prefix = inverse_prefix(lz_data[i]);
-		extrabits += extrabits_from_prefix(future_prefix);
-		lz_data_processed[lz_pro_size++] = future_prefix;
-		uint32_t back_x = lz_data[i+1] % width;
-		uint32_t back_y = lz_data[i+1] / width;
-		if(width - back_x < back_x){
-			uint8_t back_x_prefix = inverse_prefix(back_x);
-			extrabits += extrabits_from_prefix(back_x_prefix);
-			lz_data_processed[lz_pro_size++] = max_back_x - back_x_prefix;
-		}
-		else{
-			uint8_t back_x_prefix = inverse_prefix(back_x);
-			extrabits += extrabits_from_prefix(back_x_prefix);
-			lz_data_processed[lz_pro_size++] = back_x_prefix;
-		}
-		uint8_t back_y_prefix = inverse_prefix(back_y);
-		extrabits += extrabits_from_prefix(back_y_prefix);
-		lz_data_processed[lz_pro_size++] = back_y_prefix;
-		uint32_t matchlen_prefix = inverse_prefix(lz_data[i+2]);
-		extrabits += extrabits_from_prefix(matchlen_prefix);
-		lz_data_processed[lz_pro_size++] = matchlen_prefix;
+	uint8_t future_prefix = inverse_prefix(previous_match);
+	uint8_t future_extrabits = extrabits_from_prefix(future_prefix);
+	uint32_t future_extrabits_value = prefix_extrabits(previous_match);
+	if(previous_match == cache_future){
+		lz_data[lz_size - 1].future = 0;
 	}
-	uint32_t matchlen_prefix = inverse_prefix(lz_data[lz_size]);
-	extrabits += extrabits_from_prefix(matchlen_prefix);
-	lz_data_processed[lz_pro_size++] = matchlen_prefix;
+	else{
+		lz_data[lz_size - 1].future = future_prefix + 1;
+		cache_future = previous_match;
+	}
 
-	return lz_data_processed;
+	return lz_data;
 }
 
 #endif //LZ_OPTIMISER
--- prefix_coding.hpp
+++ prefix_coding.hpp
@@ -18,7 +18,49 @@
 9	24-31	3	11000-11111	10
 10	32-47	4	100000-101111	12
 11	48-63	4	110000-111111	12
+12	64	5
+13	96	5
+14	128	6
+15	192	6
+16	256	7
+17	384	7
+18	512	8
+19		8
+20	1024	9
+21		9
+22	2048	10
+23		10
+24	4096	11
+25		11
+26	8192	12
+27		12
+28	16384	13
+29		13
+30	32768	14
+31		14
+32	65536	15
+33		15
+34	131072	16
+35		16
+36	262144	17
+37		17
+38	524288	18
+39		18
+40	1048576	19
+41		19
+42	2097152	20
 */
+
+struct lz_triple{
+	uint8_t backref_x;
+	uint8_t backref_y;
+	uint8_t matchlen;
+	uint8_t future;
+	uint32_t backref_x_bits;
+	uint32_t backref_y_bits;
+	uint32_t matchlen_bits;
+	uint32_t future_bits;
+};
 
 uint8_t read_prefixcode(RansState* r, RansDecSymbol* sym, SymbolStats stats, uint8_t** fileIndex){
 	return 0;//todo
@@ -49,6 +91,20 @@ uint8_t extrabits_from_prefix(uint8_t prefix){
 	}
 	else{
 		return prefix/2 - 1;
+	}
+}
+
+uint32_t prefix_extrabits(size_t value){
+	if(value < 4){
+		return 0;
+	}
+	uint8_t magnitude  = log2_plus(value);
+	uint8_t magnitude2 = log2_plus(value - (1 << (magnitude - 1)));
+	if(magnitude2 + 1 == magnitude){
+		return value - (1 << (magnitude - 1)) - (1 << (magnitude2 - 1));
+	}
+	else{
+		return value - (1 << (magnitude - 1));
 	}
 }
 
